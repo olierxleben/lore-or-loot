b523aef94c8bd9608bf94f09f349a0e7ae0c26a5
Oliver Erxleben <oliver.erxleben@me.com>
2023-02-26 23:50:10 +0100

    added tavern example using preact interactive component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
